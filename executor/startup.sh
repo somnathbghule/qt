325bc896917ece926f28b2906f0d597299e06626
TejaswiniSoor <tejusoor@gmail.com>
2019-05-09 20:39:40 +0530

    startup script to run executor app

---
+++ executor/startup.sh
@@ -0,0 +1,13 @@
+######################################################################
+#Script to run application in ubuntu
+######################################################################
+#QT library path
+
+export QT_DIR=$HOME/Qt/5.6.3/gcc_64
+echo $QT_DIR
+export QT_XCB_GL_INTEGRATION=xcb_egl
+export PATH=$QT_DIR/bin:$PATH
+export LD_LIBRARY_PATH=$QT_DIR/lib:$LD_LIBRARY_PATH
+
+#Executable
+./Lobsty
